193e735d0be7fa95e0a203b881b771b39d436b4b
h-barad <57664155+h-barad@users.noreply.github.com>
2022-09-26 11:49:20 +0530

    3 files modified

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,10 +5,11 @@ import { AppComponent } from './app.component';
 import { NewComponent } from './new-component/nc.component';
 import { AppRoutingModule } from './app-routing.module';
 import { TableComponent } from './table-component/table.component';
+import { HomeComponent } from './home-component/hc.component';
 
 @NgModule({
   imports: [BrowserModule, FormsModule, AppRoutingModule],
-  declarations: [AppComponent, NewComponent, TableComponent],
+  declarations: [AppComponent, NewComponent, TableComponent, HomeComponent],
   bootstrap: [AppComponent],
   providers: [],
 })
